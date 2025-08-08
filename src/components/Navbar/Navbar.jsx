62e0a46612817aa3bcf2cd073f6980407de40050
T. Lanette <TLPollard4516@gmail.com>
2025-08-08 18:28:25 -0400

    Added mobile navbar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
 import { Link } from "react-router";
+import { useEffect, useState } from 'react';
 import { useNavigate } from "react-router";
 
 import Logo from '../../assets/shared/desktop/logo.svg';
+import Hamburger from '../../assets/shared/mobile/icon-hamburger.svg';
+import Close from '../../assets/shared/mobile/icon-close.svg'
 import NavList from "./NavList";
 
 
 
 export function Navbar() {
+    const [isNotMobile, setIsNotMobile] = useState(window.innerWidth > 767);
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+    const updateMobile = () => {
+        setIsNotMobile(window.innerWidth > 767);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', updateMobile);
+        return () => window.removeEventListener('resize', updateMobile);
+    });
+
+    const isNavOpen = () => {
+        setIsNavExpanded(true);
+    };
+
+    const isNavClosed = () => {
+        setIsNavExpanded(false);
+    }
 
 
     let navigate = useNavigate();
@@ -16,14 +38,45 @@ export function Navbar() {
     }
 
     return (
-        <nav aria-label='site' className='nav'>
-            <div className='logo__wrapper'>
-                <Link to='/' className='logo-link'>
-                    <img src={Logo} alt='welcome to coffee subscription' />
-                </Link>
-            </div>
-            <NavList />
+        <>
+            {isNotMobile ? (
+                <nav aria-label='site' className='nav'>
+                    <div className='logo__wrapper'>
+                        <Link to='/' className='logo-link'>
+                            <img src={Logo} alt='welcome to coffee subscription' />
+                        </Link>
+                    </div>
+                    <div className='navbar__wrapper'>
+                        <NavList />
+                    </div>
+                </nav>
+            ) : (
+                <>
+                    <nav className="navbar__mobile">
+                        <div className="logo__wrapper">
+                            <Link>
+                                <img src={Logo} alt='welcome to coffee subscription' />
+                            </Link>
+                        </div>
+                        {isNavExpanded ? (
+                            <img src={Close} className='close-icon' onClick={isNavClosed} />
+                        ) : (
+                            <img src={Hamburger} className='hamburger-icon' onClick={isNavOpen} />
+                        )}
+                    </nav>
+                    {isNavExpanded ? (
+                        <nav className='navbar__mobile--menu'>
+                            <NavList />
+                            <button className='btn' onClick={changeRoute}>
+                                Create Your PLan
+                            </button>
+                        </nav>
+                    ) : (
+                        " "
+                    )}
+                </>
+            )}
+        </>
 
-        </nav>
     )
 }
