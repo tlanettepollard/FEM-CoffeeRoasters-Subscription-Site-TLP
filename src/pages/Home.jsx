48df01e180c27ad54658ebe18ef31c6fd27bd482
T. Lanette <TLPollard4516@gmail.com>
2025-08-09 20:03:07 -0400

    Update: added font mixins
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
     return (
         <main className='main__home'>
             <SharedHero
-                className='home'
+                class='home'
                 title='Great coffee made simple'
                 desc='Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.'
                 button='Create your plan'/>
