28ce8895d6015e079f3a68fa338c5c437e99c2cd
T. Lanette <TLPollard4516@gmail.com>
2025-08-09 21:16:46 -0400

    Update: created shared works component
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/components/Shared/WorkList.jsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router";
+import { WorksItems } from "./WorksItems";
+
+export function WorkList() {
+    let navigate = useNavigate();
+    const changeRoute = () => {
+        let path = '/subscribe';
+        navigate(path);
+    }
+
+    return (
+        <section className='work container'>
+            <h2 className='work__title'>How it works</h2>
+            <div className='work__container'>
+                <WorksItems />
+                <div className='container'>
+                    <button onClick={changeRoute} className='btn primary__btn work__btn'>
+                        Create your plan
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
+}
