598647f5342cf527ae3ff6a850b21586e139b568
T. Lanette <TLPollard4516@gmail.com>
2025-08-12 19:02:27 -0400

    Added SharedHero to Subscribe page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import SharedHero from "../components/Shared/SharedHero";
 
 
 export function Subscribe() {
     return (
-        <h1>Subscribe Page</h1>
+        <main className='main__subscribe'>
+            <SharedHero class='subscribe' title='Create a plan' desc='Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.' />
+
+        </main>
     )
 }
