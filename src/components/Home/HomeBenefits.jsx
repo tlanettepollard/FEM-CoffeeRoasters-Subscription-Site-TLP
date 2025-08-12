7ba9480b4b39dcee4b72035fcf0f44e297e94883
T. Lanette <TLPollard4516@gmail.com>
2025-08-12 19:15:51 -0400

    Update: added two sections to Home page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/components/Home/HomeBenefits.jsx
@@ -0,0 +1,25 @@
+import { BenefitCards } from '../planData';
+
+export function HomeBenefits() {
+    return (
+        <section className='benefits container'>
+            <h2 className='benefits__title'>Why choose us?</h2>
+            <p className='benefits__summary'>A large part of our role is choosing which particular coffees will be featured in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.</p>
+            <ul className='benefits__cards container'>
+                {BenefitCards.map(card => {
+                    return <li className='benefit__card' key={card.id}>
+                        <div className='card__container'>
+                            <div className="card__img__container">
+                                <img className='card__img' src={card.imgUrl} alt={card.alt} />
+                            </div>
+                            <div className="card__info">
+                                <h3 className="card__title">{card.title}</h3>
+                                <p className="card__text">{card.summary}</p>
+                            </div>
+                        </div>
+                    </li>
+                })}
+            </ul>
+        </section>
+    );
+};
