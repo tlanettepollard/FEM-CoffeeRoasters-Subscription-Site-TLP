d0f53f6b2c11007a1d7e1230c12eb1374747a4f4
T. Lanette <TLPollard4516@gmail.com>
2025-08-16 20:04:49 -0400

    Updates: added divider image
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Shared/WorkList.jsx
+++ src/components/Shared/WorkList.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import { WorksItems } from "./WorksItems";
+import dividerImg from '../../assets/home/desktop/Divider.png';
 
 export function WorkList() {
     let navigate = useNavigate();
@@ -12,8 +13,9 @@ export function WorkList() {
         <section className='work container'>
             <h2 className='work__title'>How it works</h2>
             <div className='work__container'>
+                <img className='divider' src={dividerImg} alt="decorative divider" />
                 <WorksItems />
-                <div className='container'>
+                <div className='btn__container'>
                     <button onClick={changeRoute} className='btn btn-works'>
                         Create your plan
                     </button>
