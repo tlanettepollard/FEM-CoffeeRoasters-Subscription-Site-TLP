a39c17fcabe0302be8864de277dc1b30617f2aad
T. Lanette <TLPollard4516@gmail.com>
2025-08-30 18:06:35 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/About/AboutLocation.jsx
+++ src/components/About/AboutLocation.jsx
@@ -7,20 +7,16 @@ export function AboutLocation() {
             <ul className='location__content'>
                 {Countries.map(site => {
                     return <li key={site.id} className='location__list'>
-                        <div className='location__img__container'>
-                            <img className='location__img' src={site.imgUrl} width={site.width} height={site.height} alt={site.alt} />
-                        </div>
-                        <div className='location__details'>
-                            <address>
-                                <span className='location__title'>{site.country}</span>
-                                <p className='location__address'>
-                                    {site.street} <br />
-                                    {site.city} <br />
-                                    {site.postcode} <br />
-                                    {site.phone} <br />
-                                </p>
-                            </address>
-                        </div>
+                        <img className='location__img' src={site.imgUrl} width={site.width} height={site.height} alt={site.alt} />
+                        <address>
+                            <span className='location__title'>{site.country}</span>
+                            <p className='location__address'>
+                                {site.street} <br />
+                                {site.city} <br />
+                                {site.postcode} <br />
+                                {site.phone} <br />
+                            </p>
+                        </address>
                     </li>
                 })}
             </ul>
