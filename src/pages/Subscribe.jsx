40148517148a5df0018bdc28c2b417d272763cc9
T. Lanette <TLPollard4516@gmail.com>
2025-08-30 19:21:56 -0400

    Started styling section1 on subscribe page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import SharedHero from "../components/Shared/SharedHero";
 import { WorksItems } from "../components/Shared/WorksItems";
+import dividerImg from '../assets/home/desktop/Divider.png';
 import { Plans } from "../components/Subscribe/Plans";
 
 
 export function Subscribe() {
     return (
-        <main className='main__subscribe'>
+        <main className='main__subscribe container'>
             <SharedHero class='subscribe' title='Create a plan' desc='Build a subscription plan that best fits your needs. We offer an assortment of the best artisan coffees from around the globe delivered fresh to your door.' />
-            <section className="works__container subscribe__works__container container">
-                <WorksItems />
+            <section className="work">
+                <div>
+                    <img className='divider' src={dividerImg} alt="decorative divider" />
+                </div>
+                <div className="work__container">
+                    <WorksItems />
+                </div>
+
             </section>
             <Plans />
 
